6c18cab82d49d0bbe09d2b74c7f1c5f40ccd7219
biranchi007 <biranchi007@gmail.com>
2020-04-11 12:55:26 +0530

    2 files modified, 3 added

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import React from "react"
+import React from "react";
+import TodoItem from "./TodoItem.js";
+import ContactCard from "./ContactCard.js";
 
-function App()
-{
-  return(
-    <div>
-      <input type = "checkbox" />
-      <p> Placeholder text here</p>
-      <input type = "checkbox" />
-      <p> Placeholder text here</p>
-      <input type = "checkbox" />
-      <p> Placeholder text here</p>
-
+function App() {
+  return (
+    <div className="todo-list">
+      <h1>ToDO Items here</h1>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <h1>Contact Cards here</h1>
+      <ContactCard name = "First Kitten" imgUrl="/>
+      <ContactCard />
+      <ContactCard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ ContactCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function ContactCard()
+{
+  return (
+    <div>
+      <div className = "contact-card">
+        <img src="http://placekitten.com/300/200"/>
+        <h3> Mr. Whiskerson </h3>
+        <p> Phone : 111 111 1111</p>
+        <p> Email : a@b.com </p>
+      </div>
+      <div className = "contact-card">
+        <img src="http://placekitten.com/300/200"/>
+        <h3> Mr. Whiskerson </h3>
+        <p> Phone : 111 111 1111</p>
+        <p> Email : a@b.com </p>
+      </div>
+    </div>
+
+  )
+}
+
+export default ContactCard
